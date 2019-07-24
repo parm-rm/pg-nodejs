e6fc83782020942e4ee8bc0a0b12dc72e749c7a7
Parminder Gill <parminder@roadmunk.com>
2019-07-24 16:11:04 -0400

    removing custom nodejs install

--- image/create.sh
+++ image/create.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SCRIPT_DIRECTORY="$(cd "$(dirname "$0")";pwd)"
-docker build --compress "--tag=pg-nodejs:latest" "$SCRIPT_DIRECTORY"
+docker build --compress "--tag=parmrm/pg-nodejs:latest" "$SCRIPT_DIRECTORY"
